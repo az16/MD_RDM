Corrected calculation of weighted components

network/RDM_Net.py
@@ -80,11 +80,11 @@ class DepthEstimationNet(BaseModel):
         x_d9 = self.d_9(x)#relative
         
         #get fine-detail maps for each depth map
-        f_d1 = cp.decompose_depth_map([], x_d1, 3)
-        f_d6 = cp.decompose_depth_map([], x_d6, 3, relative_map=True)
-        f_d7 = cp.decompose_depth_map([], x_d7, 4, relative_map=True)
-        f_d8 = cp.decompose_depth_map([], x_d8, 5, relative_map=True)
-        f_d9 = cp.decompose_depth_map([], x_d9, 6, relative_map=True)
+        f_d1 = cp.decompose_depth_map([], x_d1, 3)[::-1]
+        f_d6 = cp.decompose_depth_map([], x_d6, 3, relative_map=True)[::-1]
+        f_d7 = cp.decompose_depth_map([], x_d7, 4, relative_map=True)[::-1]
+        f_d8 = cp.decompose_depth_map([], x_d8, 5, relative_map=True)[::-1]
+        f_d9 = cp.decompose_depth_map([], x_d9, 6, relative_map=True)[::-1]
 
         #optimization of components
         # y_hat = cp.relative_fine_detail_matrix([f_d1, f_d6, f_d7, f_d8, f_d9])
@@ -386,7 +386,7 @@ class Weights:
         return self.weight_list[index]
 
     def _make_weightvector_list_(self, sizes):
-        return [torch.randn((size,1)) for size in sizes]
+        return [torch.randn((size,1), requires_grad=True) for size in sizes]
 
 def _make_wsm_vertical_(in_channels, out_channels, kernel_size, stride):
     """Stride has to be chosen in a way that only one convolution is performed
@@ -473,23 +473,18 @@ def debug(container, id):
     print("\n")
 
 if __name__ == "__main__":
-    dataset = [(torch.randn((4,3,226,226)),torch.randn((4,1,128,128))) for x in range(50)]
-    #dn = torch.randn((16,1,16,16))
-    #dn_1 = cp.resize(dn,8)
-    w_x = Weights([1,5,5,5,3,2,1,0])
-    EPOCHS = range(10)
+    ground_truth = torch.randn((4,1,128,128))
+    test_input = torch.randn((4,3,226,226))
+    lr = 0.001
+    weight_layer = Weights([1,5,5,5,3,2,1,0])
     network = DepthEstimationNet()
-    for epoch in EPOCHS:
-        for input_batch in dataset:
-            d1,d6,d7,d8,d9 = network(input_batch[0])
-            #debug(d1, 1)
-            #debug(d6, 6)
-            #debug(d7, 7)
-            #debug(d8, 8)
-            #debug(d9, 9)
-            y_hat = cp.relative_fine_detail_matrix([d1, d6, d7, d8, d9])
-            y = cp.decompose_depth_map([], input_batch[1], 7)
-            optimal_candidates = cp.make_optimal_component(y_hat, y)
+    d1, d6, d7, d8, d9 = network(test_input)
+    y_hat = cp.relative_fine_detail_matrix([d1, d6, d7, d8, d9])
+    y = cp.decompose_depth_map([], ground_truth, 7)[::-1]
+    y_hat = cp.make_pred(weight_layer.weight_list, y_hat)
+    for i in range(7):
+        print(y_hat[i].shape, y[i].shape)
+    #optimal_candidates = cp.optimize_components(weight_layer, lr, y_hat, y)
 
 
     

network/computations.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import torch 
 import torch.nn as nn
+import math
 from scipy.sparse import linalg  as sp
 from scipy import sparse as s
 from statistics import geometric_mean as gm
@@ -313,18 +314,35 @@ def relative_fine_detail_matrix(fine_detail_rows):
     slots = [[] for x in range(7)]
     
     #put candidates of the same size together in lists
-    for i in range(7):
-        for component in fine_detail_rows:
-            if len(component[0]) > 0:
-                if i==0 and component[1]:
-                    slots[i].append(component[0].pop(0))
-                elif i>0:
-                    slots[i].append(component[0].pop(0))
+    for row in fine_detail_rows:
+        for fine_detail_map in row:
+            idx = idx_from_size(fine_detail_map)
+            slots[idx].append(fine_detail_map)
     
     #create matrix from candidates
     fine_detail_matrices = [make_matrix(x) for x in slots]
 
     return fine_detail_matrices
+
+def idx_from_size(fine_detail_map):
+    B,C,H,W = fine_detail_map.size()
+
+    if H == 1:
+        return 0
+    elif H == 2:
+        return 1
+    elif H == 4:
+        return 2
+    elif H == 8:
+        return 3
+    elif H == 16:
+        return 4
+    elif H == 32:
+        return 5
+    elif H == 64:
+        return 6
+    elif H == 128:
+        return 7
 
 def make_matrix(list_of_candidates):
     """
@@ -334,12 +352,28 @@ def make_matrix(list_of_candidates):
     list_of_candidates - all fine detail components of same size in a list
     returns - matrix of all fine detail components
     """
-    B,C,H,W = list_of_candidates[0].size
+    B,C,H,W = list_of_candidates[0].size()
     candidates = [x.view(B,1,C*H*W) for x in list_of_candidates]
-    return torch.squeeze(torch.cat(candidates, dim=1))
+    return torch.cat(candidates, dim=1)
 
-def make_optimal_component(yhat, y):
+def optimize_components(weights, lr, yhat, y):
+    pred = make_pred(weights, yhat)
+    loss = squared_err(yhat,y)
+    loss.backward()
+    with torch.no_grad():
+        for i in range(len(weights.weightlist)) :
+            weights.update(i, lr, loss)
     return 0
+
+def make_pred(w, A):
+    for i in range(len(A)):
+        B,M = A[i].shape[0], A[i].shape[2]
+        tmp = torch.zeros((B, M, 1))
+
+        for b in range(A[i].shape[0]):
+           tmp[b]  = A[i][b].T.float()@w[i].float()
+        A[i] = tmp.view(B,1,int(math.sqrt(M)),int(math.sqrt(M)))
+    return A 
 
 def squared_err(yhat,y):
     return torch.sum(torch.abs(y-yhat)**2, 1)
@@ -351,7 +385,6 @@ def debug_recombination():
     container = [torch.randint(1,10,(1,1,2**x,2**x)) for x in range(7)]
 
     return container
-
 
 if __name__ == "__main__":
     for dmap in debug_recombination():